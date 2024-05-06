9b028b06f8bc213497350c8337261c2e10f9821e
syedishaq13129 <122854504+syedishaq13129@users.noreply.github.com>
2024-05-06 17:00:23 +0530

    Update after-install.sh

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -3,7 +3,7 @@
 
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://codedeployanil0605/deploytest1.zip /home/ec2-user
+aws s3 cp s3://codedeployanil0605/deploytest2.zip /home/ec2-user
 
 # Ensure the ownership permissions are correct.
 # chown -R tomcat:tomcat /usr/local/tomcat9/webapps
