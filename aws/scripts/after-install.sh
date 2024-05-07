dd4cc52dbcf82d5f998d37e7c08beeccd1bfc596
syedishaq13129 <122854504+syedishaq13129@users.noreply.github.com>
2024-05-07 16:25:00 +0530

    Update after-install.sh

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -4,8 +4,11 @@
 # Set the release tag name from the environment variable
 #RELEASE_TAG_NAME=$CODEDEPLOY_ENV_RELEASE_TAG_NAME
 
+# Fetch the latest release tag from the GitHub API
+LATEST_RELEASE_TAG=$(curl -s "https://api.github.com/repos/syedishaq13129/deploy-a-amazon-ec2-using-githubaction/releases/latest" | grep -o '"tag_name": "[^"]*' | grep -o '[^"]*$')
+
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://codedeployanil0605/syed_4.0.zip /home/ec2-user
+aws s3 cp s3://codedeployanil0605/$LATEST_RELEASE_TAG.zip /home/ec2-user
 
 # Ensure the ownership permissions are correct.
 # chown -R tomcat:tomcat /usr/local/tomcat9/webapps
