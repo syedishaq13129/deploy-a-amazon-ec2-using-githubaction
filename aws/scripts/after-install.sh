c5244b39421c175293c02fa70d2a9708ce245ede
syedishaq13129 <122854504+syedishaq13129@users.noreply.github.com>
2024-05-07 15:46:57 +0530

    Update after-install.sh

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -2,10 +2,10 @@
 # set -xe
 
 # Set the release tag name from the environment variable
-RELEASE_TAG_NAME=$CODEDEPLOY_ENV_RELEASE_TAG_NAME
+#RELEASE_TAG_NAME=$CODEDEPLOY_ENV_RELEASE_TAG_NAME
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://codedeployanil0605/${RELEASE_TAG_NAME} /home/ec2-user
+aws s3 cp s3://codedeployanil0605/syed_3.0 /home/ec2-user
 
 # Ensure the ownership permissions are correct.
 # chown -R tomcat:tomcat /usr/local/tomcat9/webapps
