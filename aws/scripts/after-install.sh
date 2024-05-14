fd4ccda45826f5acea3db5dd02c4e23157392a26
syedishaq13129 <122854504+syedishaq13129@users.noreply.github.com>
2024-05-14 17:42:24 +0530

    Update after-install.sh

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# set -xe
-
-# Set the release tag name from the environment variable
-#RELEASE_TAG_NAME=$CODEDEPLOY_ENV_RELEASE_TAG_NAME
 
 # Fetch the latest release tag from the GitHub API
 LATEST_RELEASE_TAG=$(curl -s "https://api.github.com/repos/syedishaq13129/deploy-a-amazon-ec2-using-githubaction/releases/latest" | grep -o '"tag_name": "[^"]*' | grep -o '[^"]*$')
@@ -15,6 +11,3 @@ cd /home/ec2-user
 
 # Remove all files except 2 Recently modified files
 ls -t | tail -n +3 | xargs rm -f
-
-# Ensure the ownership permissions are correct.
-# chown -R tomcat:tomcat /usr/local/tomcat9/webapps
