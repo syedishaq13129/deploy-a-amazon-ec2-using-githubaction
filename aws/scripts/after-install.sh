3ea911d7ad2279435d3ccbe7bca6670e83164263
syedishaq13129 <122854504+syedishaq13129@users.noreply.github.com>
2024-05-15 12:05:47 +0530

    Update after-install.sh

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -4,7 +4,7 @@
 LATEST_RELEASE_TAG=$(curl -s "https://api.github.com/repos/syedishaq13129/deploy-a-amazon-ec2-using-githubaction/releases/latest" | grep -o '"tag_name": "[^"]*' | grep -o '[^"]*$')
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://codedeployanil0605/$LATEST_RELEASE_TAG.zip /home/ec2-user
+aws s3 cp s3://deepak30101997/$LATEST_RELEASE_TAG.zip /home/ec2-user
 
 # Change the Working Directory
 cd /home/ec2-user
